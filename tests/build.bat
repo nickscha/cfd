71a49a814eb03b1df27305fc40b62f5ba25eb257
nickscha <nick@scharnbeck.de>
2025-08-09 17:27:47 +0200

    [BUILD] optimization flags

--- tests/build.bat
+++ tests/build.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set DEF_FLAGS_COMPILER=-std=c89 -pedantic -Wall -Wextra -Werror -Wvla -Wconversion -Wdouble-promotion -Wsign-conversion -Wuninitialized -Winit-self -Wunused -Wunused-macros -Wunused-local-typedefs
+set DEF_FLAGS_COMPILER=-std=c89 -march=native -mtune=native -pedantic -Wall -Wextra -Werror -Wvla -Wconversion -Wdouble-promotion -Wsign-conversion -Wuninitialized -Winit-self -Wunused -Wunused-macros -Wunused-local-typedefs
 set DEF_FLAGS_LINKER=
 set SOURCE_NAME=cfd_test
 
-cc -s -O2 %DEF_FLAGS_COMPILER% -o %SOURCE_NAME%.exe %SOURCE_NAME%.c %DEF_FLAGS_LINKER%
+cc -s -O3 %DEF_FLAGS_COMPILER% -o %SOURCE_NAME%.exe %SOURCE_NAME%.c %DEF_FLAGS_LINKER%
 %SOURCE_NAME%.exe
